6723ae17e95b3b91c13a3da64ab06268ee9969a5
Chris <chrislaird89@gmail.com>
2023-01-31 10:46:56 -0500

    Added code

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,5 @@
 package exercise1;
-
+import java.util.*;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -12,15 +12,21 @@ public class CardTrick {
     
     public static void main(String[] args) {
         
-        Card[] hand = new Card[7];
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        Card[] cards = new Card[7];
+        int cardValue;
+        int cardSuit;
 
-        for (int i = 0; i < hand.length; i++) {
+        for(int i = 0; i < cards.length; i++) {
             Card card = new Card();
             //card.setValue(insert call to random number generator here)
-            // 
+            card.setValue(random.nextInt(13) + 1);
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            cards[i] = card;
         }
 
         // insert code to ask the user for Card value and suit, create their card
@@ -30,9 +36,21 @@ public class CardTrick {
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
         // 
         // Then loop through the cards in the array to see if there's a match.
+        System.out.print("Input a card value from 1-13(1 being ace, 11 being"
+                + " jack, 12 being queen and 13 being king: ");
+        cardValue = input.nextInt();
+        System.out.print("Input a suit value between 1-4 (1 being Hearts, 2"
+                + " being Diamonds, 3 being Spades and 4 being clubs): ");
+        cardSuit = input.nextInt() - 1;
+        
         
         // If the guess is successful, invoke the printInfo() method below.
-        
+        for(int i = 0; i < cards.length; i++){
+            System.out.println(cards[i].getValue());
+        System.out.println(cards[i].getSuit() );
+            if(cards[i].getValue() == cardValue && cards[i].getSuit().equals(Card.SUITS[cardSuit]))
+                    printInfo();
+        }       
     }
 
     /**
